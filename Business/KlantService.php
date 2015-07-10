ee8028511fec6886525628d41f9d0201d34a9938
Sarah Heindryckx <sarah.heindryckx@hotmail.com>
2015-07-10 10:11:55 +0200

    Business

---
+++ Business/KlantService.php
@@ -0,0 +1,26 @@
+<?php
+//Sarah: check this 
+
+require_once ("datalaag/klantDAO.php");
+
+class klantService{
+    public function klantToevoegen($naam, $voornaam, $straat, $huisnummer,
+            $postcode, $woonplaats, $email, $wachtwoord, $tel) {
+        $klantDAO=new klantDAO();
+        $klantDAO->create($naam, $voornaam, $straat, $huisnummer,
+            $postcode, $woonplaats, $email, $wachtwoord, $tel);
+        
+    }
+  
+    
+    /*public function klantTonen($id) {
+        $klantDAO= new klantDAO();
+        $klant=$klantDAO->getById($id);
+        return $klant;
+    }
+    public function klantDelete($id) {
+     $klantDAO= new klantDAO();
+     $klantDAO->delete($id);
+    }*/
+    
+}
